661a8bf7356e29ab112c4905b879e5accb769dcf
sirgoofiguskid <sirgoofiguskid@gmail.com>
2021-09-25 15:22:01 -0500

    added file output, added periods, adjusted newlines

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
+import sys
 import random
+from datetime import datetime
 
 verbose = False # Set to True for debugging information
+output = "\n"
 
 notFinished = True
 
-print()
-print("Welcome the pick-name-from-the-hat game!")
+output += "Welcome the pick-name-from-the-hat game!\n"
 
 while (notFinished):
   Hat = { # The names in a hat. The key is the name and the value is the person that name cannot pick (only used in Choosers).
@@ -22,8 +24,8 @@ while (notFinished):
   Selections = dict() # Records who chose whom
 
 
-  print()
-  print ("Placing these names into the hat:")
+  output += "\n"
+  output += "Placing these names into the hat:\n"
   counter = 1
   for name,nameCannotChoose in Hat.items():
     if (counter == len(Hat)):
@@ -34,22 +36,22 @@ while (notFinished):
       suffix = ","
 
     if(nameCannotChoose == ""):
-      print(prefix + name + suffix)
+      output += prefix + name + suffix + "\n"
     else:
-      print (prefix + name + " (who can't choose " + nameCannotChoose + ")" + suffix)
+      output += prefix + name + " (who can't choose " + nameCannotChoose + ")" + suffix + "\n"
     counter += 1
 
-  print()
-  print("Now to pick the names!")
+  output += "\n"
+  output += "Now to pick the names!"
 
   numChoosers = len(Choosers)
 
   for i in range (0,numChoosers): # Randomly select who choooses a name from the hat
 
     if (verbose):
-      print("Still to choose: ")
+      output += "Still to choose: \n"
       for chooser in Choosers:
-        print(chooser)
+        output += chooser
 
     # Chose who will draw a name from the hat
     chooser, chooserCannotChoose = random.choice(list(Choosers.items()))
@@ -58,32 +60,32 @@ while (notFinished):
     while(stillChoosing):
       # Draw from the hat
       drawnName, drawnNameCannotChoose = random.choice(list(Hat.items()))
-      print("It is " + chooser + "'s turn..." + chooser + " reaches into the hat...and draws " + drawnName + "!")
+      output += "It is " + chooser + "'s turn..." + chooser + " reaches into the hat...and draws " + drawnName + ". "
       if (chooser == drawnName):
         if(i == numChoosers - 1):
-          print("And finally, " + chooser + " will have to choose " + drawnName + " which means no Christmas for " + chooser + ". Too bad for you. :(     Just kidding! Let's just start the whole thing over...")
+          output += "And finally, " + chooser + " will have to choose " + drawnName + " which means no Christmas for " + chooser + ". Too bad for you. :(     Just kidding! Let's just start the whole thing over...\n"
           stillChoosing = False
         else:
-          print("But " + chooser + " clearly has to pick somebody else! Back in the hat.")
+          output += "But " + chooser + " clearly has to pick somebody else! Back in the hat."
       elif(chooser == chooserCannotChoose):
         if(i == numChoosers - 1):
-          print("Well that's great..." + chooser + " will have to choose " + drawnName + " which is against the rules, so " + drawnName + " won't get any presents. Bummer. :(     Just kidding! Let's just start the whole thing over...")
+          output += "Well that's great..." + chooser + " will have to choose " + drawnName + " which is against the rules, so " + drawnName + " won't get any presents. Bummer. :(     Just kidding! Let's just start the whole thing over...\n"
           stillChoosing = False
         else:
-          print("But the rules say that " + chooser + " cannot choose " + chooserCannotChoose + "! Back in the hat.")
+          output += "But the rules say that " + chooser + " cannot choose " + chooserCannotChoose + "! Back in the hat.\n"
       else:
-        print("This is A OK!")
+        output += "This is A OK!\n"
         stillChoosing = False
         Selections[chooser] = drawnName
         del Hat[drawnName]
         if(i == numChoosers - 1):
-          print("Congratulations! We finished an entire game and didn't break any rules!")
+          output += "Congratulations! We finished an entire game and didn't break any rules!\n"
           notFinished = False
         
     del Choosers[chooser] # Make sure they don't choose again
 
-print("The final results: ")
-print("**************************************************************")
+output += "The final results: \n"
+output += "**************************************************************\n"
 counter = 1
 for chooser,chosen in Selections.items():
   if (counter == len(Selections)):
@@ -93,7 +95,16 @@ for chooser,chosen in Selections.items():
     prefix = ""
     suffix = ","
 
-  print (prefix + chooser + " => is giving a gift to => " + chosen + suffix)
+  output += prefix + chooser + " => is giving a gift to => " + chosen + suffix + "\n"
   counter += 1
 
-print("**************************************************************")
+output += "**************************************************************\n"
+
+date = datetime.now().strftime("%Y_%m_%d-%I_%M_%S_%p")
+original_stdout = sys.stdout
+with open("secret_santa_" + str(date) + ".txt", 'w') as f:
+    sys.stdout = f # Change the standard output to the file we created.
+    print(output)
+    sys.stdout = original_stdout
+    
+print(output)
